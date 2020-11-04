dfb3c6dd2b726b201f599c90f168ef79246b16e8
vijishankar <67778907+vijishankar@users.noreply.github.com>
2020-11-04 13:30:16 +0530

    Update db-reset.ps1

--- config/db-reset.ps1
+++ config/db-reset.ps1
@@ -1,27 +1,27 @@
 param(
-        [Parameter(Mandatory=$true, ValueFromPipeline=$true)] [String] $ResourceGroupName,
-	[Parameter(Mandatory=$true, ValueFromPipeline=$true)] [String] $SeverName,
-	[Parameter(Mandatory=$true, ValueFromPipeline=$true)] [String] $KeyVaultName,
-	[Parameter(Mandatory=$true, ValueFromPipeline=$true)] [String] $SecretName,
-	[Parameter(Mandatory=$true, ValueFromPipeline=$true)] [String] $SecretValue
-	 )
+[Parameter(Mandatory=$true, ValueFromPipeline=$true)] [String] $ResourceGroupName,
+[Parameter(Mandatory=$true, ValueFromPipeline=$true)] [String] $SeverName,
+[Parameter(Mandatory=$true, ValueFromPipeline=$true)] [String] $KeyVaultName,
+[Parameter(Mandatory=$true, ValueFromPipeline=$true)] [String] $SecretName,
+[Parameter(Mandatory=$true, ValueFromPipeline=$true)] [String] $SecretValue
+)
 
 #$ResourceGroupName = "DB-Rest-Passwd"
 #$SeverName = "sqlserve123456"
 #$KeyVaultName = "secret-db-passwd"
 #$SecretName = "password"
 
- $sqlserver = Get-AzSqlServer -ResourceGroupName $ResourceGroupName -ServerName $SeverName -ErrorVariable notPresent -ErrorAction SilentlyContinue
- $keyVault= Get-AzKeyVault -VaultName $keyVaultName -ErrorVariable notPresent -ErrorAction SilentlyContinue
- 
+$sqlserver = Get-AzSqlServer -ResourceGroupName $ResourceGroupName -ServerName $SeverName -ErrorVariable notPresent -ErrorAction SilentlyContinue
+$keyVault= Get-AzKeyVault -VaultName $keyVaultName -ErrorVariable notPresent -ErrorAction SilentlyContinue
+
 
 function changePassword()
 {
 if($sqlserver)
-          {
+{
 
 #Generating Random Password from RandomCharacters Method
-$Resetpassword = -join((65..90) + (97..122) + (58..64) + (58..64) + (32..47)  | Get-Random -Count 15 | % {[char]$_})
+$Resetpassword = -join((65..90) + (97..122) + (58..64) + (58..64) + (32..47) | Get-Random -Count 15 | % {[char]$_})
 Write-Host $Resetpassword
 Write-Output $Resetpassword
 
@@ -32,13 +32,13 @@ Set-AzSqlServer -ResourceGroupName $ResourceGroupName -ServerName $SeverName -Sq
 
 Write-Host $newpsswd
 Write-Output $newpsswd
-			
-			if($SecretValue -ne $SecureStringpwd)   
-			{
-		          Set-AzKeyVaultSecret -VaultName $keyVaultName -Name $SecretName -SecretValue $SecureStringpwd 
-	                  Write-Output "SecretValue updated"
-	                }
-          }
+
+if($SecretValue -ne $SecureStringpwd)
+{
+Set-AzKeyVaultSecret -VaultName $keyVaultName -Name $SecretName -SecretValue $SecureStringpwd
+Write-Output "SecretValue updated"
+}
+}
 
 else{
 Write-Host "There is no Sql Server in the given Resource Group"
